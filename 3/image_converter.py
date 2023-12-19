0d8cc94484e2073cc170f872ba6f6bd2460b2525
LAMPA23 <bimeebim@gmail.com>
2023-12-19 21:08:24 +0200

    -

--- 3/image_converter.py
+++ 3/image_converter.py
@@ -2,21 +2,23 @@ import cv2
 import numpy as np
 from PIL import Image
 
+
+
+# Color space conversion functions
+
 def get_color_space(image_path):
     return Image.open(image_path).mode
 
-
-def BGR_to_CMY(image):
+def RGB_to_CMY(image):
     blue, green, red = cv2.split(image)
     cyan = 255 - red
     magenta = 255 - green
     yellow = 255 - blue
     return cv2.merge([cyan, magenta, yellow])
 
-
-def BGR_to_HSI(image):
+def RGB_to_HSI(image):
     image_normalized = image / 255.0
-    blue, green, red = cv2.split(image_normalized)
+    red, green, blue = cv2.split(image_normalized)
     intensity = (red + green + blue) / 3.0
     denominator = np.sqrt((red - green)**2 + (red - blue) * (green - blue))
     denominator[denominator == 0] = 1e-5  # To avoid division by zero
@@ -28,9 +30,91 @@ def BGR_to_HSI(image):
     return cv2.merge([hue, saturation, intensity])
 
 
-def to_smooth(image, sigma=1.0):
+
+
+
+
+# Smoothing functions
+
+def smooth_RGB(image, sigma=1.0):
     red_channel, green_channel, blue_channel = cv2.split(image)
     smooth_blue = cv2.GaussianBlur(blue_channel, (0, 0), sigma)
     smooth_green = cv2.GaussianBlur(green_channel, (0, 0), sigma)
     smooth_red = cv2.GaussianBlur(red_channel, (0, 0), sigma)
     return cv2.merge([smooth_red, smooth_green, smooth_blue])
+
+def smooth_HSV(image, smoothing_factor=10):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    image[:,:,2] = cv2.blur(image[:,:,2], (smoothing_factor,smoothing_factor))
+    return cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+
+def smooth_Lab(image, smoothing_factor=10):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
+    image[:,:,0] = cv2.blur(image[:,:,0], (smoothing_factor,smoothing_factor))
+    return cv2.cvtColor(image, cv2.COLOR_Lab2RGB)
+
+
+
+
+
+# Increase sharpness functions
+
+def increase_sharpness_RGB(image):
+    sharpness_filater = np.array([
+        [-1,-1,-1],
+        [-1, 9,-1],
+        [-1,-1,-1]
+    ])
+    return cv2.filter2D(image, -1, sharpness_filater)
+
+def increase_sharpness_HSV(image):
+    sharpness_filater = np.array([
+        [ 0, 2, 0],
+        [ 2,-8, 2],
+        [ 0, 2, 0]
+    ])
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    h_channel, s_channel, v_channel = cv2.split(hsv_image)
+    v_channel = cv2.subtract(v_channel, cv2.filter2D(v_channel, -1, sharpness_filater))
+    hsv_image_sharp = cv2.merge([h_channel, s_channel, v_channel])
+    return cv2.cvtColor(hsv_image_sharp, cv2.COLOR_HSV2RGB)
+
+def increase_sharpness_Lab(image):
+    sharpness_filater = np.array([
+        [ 0, 1, 0],
+        [ 1,-4, 1],
+        [ 0, 1, 0]
+    ])
+    lab_image = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
+    l_channel = lab_image[:,:,0]
+    sharpness_l_channel = cv2.subtract(l_channel, cv2.filter2D(l_channel, -1, sharpness_filater))
+    lab_image[:,:,0] = sharpness_l_channel
+    return cv2.cvtColor(lab_image, cv2.COLOR_Lab2RGB)
+
+
+
+
+
+# Equalization functions
+
+def equaliz_RGB(image):
+    red_channel, green_channel, blue_channel = cv2.split(image)
+    equalized_red_channel = cv2.equalizeHist(red_channel)
+    equalized_green_channel = cv2.equalizeHist(green_channel)
+    equalized_blue_channel = cv2.equalizeHist(blue_channel)
+    return cv2.merge((equalized_red_channel, equalized_green_channel, equalized_blue_channel))
+
+def equaliz_HSV(image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    h_channel, s_channel, v_channel = cv2.split(image)
+    equalized_s_channel = cv2.equalizeHist(s_channel)
+    equalized_v_channel = cv2.equalizeHist(v_channel)
+    image = cv2.merge((h_channel, equalized_s_channel, equalized_v_channel))
+    return cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+
+def equaliz_Lab(image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
+    l_channel, a_channel, b_channel = cv2.split(image)
+    equalized_l_channel = cv2.equalizeHist(l_channel)
+    image = cv2.merge((equalized_l_channel, a_channel, b_channel))
+    return cv2.cvtColor(image, cv2.COLOR_Lab2RGB)
--- 3/main.py
+++ 3/main.py
@@ -8,6 +8,7 @@ import image_show as im_show
 import image_converter as im_conv
 
 
+# Folder correcting
 os.system('cls')
 for dir in os.listdir():
     if dir != 'original_images':
@@ -15,26 +16,51 @@ for dir in os.listdir():
 os.system('cls')
 
 
+# Get origin image
 image_1_path = 'original_images/pic1.png'
 image_2_path = 'original_images/pic2.png'
 original_image_1 = cv2.imread(image_1_path)
 original_image_2 = cv2.imread(image_2_path)
-print(f'pic1.png type mode is {im_conv.get_color_space(image_1_path)}')
-print(f'pic2.png type mode is {im_conv.get_color_space(image_2_path)}')
+print(f'pic1.png color space is {im_conv.get_color_space(image_1_path)}')
+print(f'pic2.png color space is {im_conv.get_color_space(image_2_path)}')
 
 
 # Task #1
 im_show.save_image(original_image_1, 'Original (RGB)', 'Task #1')
 im_show.save_image(cv2.cvtColor(original_image_1, cv2.COLOR_RGB2BGR), 'RGB to BGR', 'Task #1')
 im_show.save_image(cv2.cvtColor(original_image_1, cv2.COLOR_RGB2XYZ), 'RGB to XYZ', 'Task #1')
-im_show.save_image(im_conv.BGR_to_CMY(original_image_1), 'RGB to CMY', 'Task #1')
+im_show.save_image(im_conv.RGB_to_CMY(original_image_1), 'RGB to CMY', 'Task #1')
 im_show.save_image(cv2.cvtColor(original_image_1, cv2.COLOR_RGB2HSV), 'RGB to HSV', 'Task #1')
-im_show.save_image(im_conv.BGR_to_HSI(original_image_1), 'RGB to HSI', 'Task #1')
+im_show.save_image(im_conv.RGB_to_HSI(original_image_1), 'RGB to HSI', 'Task #1')
 im_show.save_image(cv2.cvtColor(original_image_1, cv2.COLOR_RGB2Lab), 'RGB to Lab', 'Task #1')
 im_show.save_image(cv2.cvtColor(original_image_1, cv2.COLOR_RGB2Luv), 'RGB to Luv', 'Task #1')
 
 
 # Task #2
 im_show.save_image(original_image_2, 'Original', 'Task #2')
-im_show.save_image(im_conv.to_smooth(original_image_2), 'Smooth', 'Task #2')
+im_show.save_image(im_conv.smooth_RGB(original_image_2), 'Smooth', 'Task #2')
+
+
+# Task #3
+im_show.save_image(original_image_2, 'Original', 'Task #3')
+im_show.save_image(im_conv.smooth_HSV(original_image_2), 'Smooth HSV', 'Task #3')
+im_show.save_image(im_conv.smooth_Lab(original_image_2), 'Smooth Lab', 'Task #3')
+
+
+# Task #4
+im_show.save_image(original_image_2, 'Original', 'Task #4')
+im_show.save_image(im_conv.increase_sharpness_RGB(original_image_2), 'increase sharpness RGB', 'Task #4')
+
+
+# Task #5
+im_show.save_image(original_image_2, 'Original', 'Task #5')
+im_show.save_image(im_conv.increase_sharpness_HSV(original_image_2), 'increase_sharpness_HSV', 'Task #5')
+im_show.save_image(im_conv.increase_sharpness_Lab(original_image_2), 'increase_sharpness_Lab', 'Task #5')
+
+
+# Task #6
+im_show.save_image(original_image_2, 'Original', 'Task #6')
+im_show.save_image(im_conv.equaliz_RGB(original_image_2), 'Equalized RGB', 'Task #6')
+im_show.save_image(im_conv.equaliz_HSV(original_image_2), 'Equalized HSV', 'Task #6')
+im_show.save_image(im_conv.equaliz_Lab(original_image_2), 'Equalized Lab', 'Task #6')
 
